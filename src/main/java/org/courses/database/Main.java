6749ae73bcb2b1bc4af55d2c36da9f67e5105fa8
Josue Aguilar <josueaguilarjnai@gmail.com>
2025-05-15 17:03:16 -0600

    feat: persisting data with hibernate

--- src/main/java/org/courses/database/Main.java
+++ src/main/java/org/courses/database/Main.java
@@ -1,7 +1,39 @@
 package org.courses.database;
 
+import org.courses.database.dto.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
+        Configuration cfg = new Configuration();
+        cfg = cfg.configure("META-INF/hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
 
+        Employee firstEmployee = new Employee(1121,
+                "Pedro",
+                "Del Sagrado Corazón de Jesus",
+                "Manager",
+                120000
+                );
+        Employee secondEmployee = new Employee(1121,
+                "Pablo",
+                "Amiguos",
+                "Analyst",
+                100000
+        );
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(firstEmployee);
+        session.save(secondEmployee);
+
+        transaction.commit();
+
+        session.close();
+        factory.close();
     }
 }
